165a61630dfb6a3afd630dd9197f4bc6b2c89a3f
Dominik2122 <dominik.iwanek@gmail.com>
2021-02-10 14:36:51 +0100

    Create ad1.py

---
+++ ad1.py
@@ -0,0 +1,24 @@
+file = open("docx.txt", "r")
+
+lista = []
+x = file.read()
+x = x.split("\n")
+ilosc_hasel = 0
+for i in x:
+
+    i = i.split(" ")
+    szukana = i[1][0]
+    ilosc = i[0]
+    ilosc = ilosc.split("-")
+    a = int(ilosc[0])
+    b = int(ilosc[1])
+    hop = 0
+    if i[2][a-1] == szukana and i[2][b-1] != szukana:
+        ilosc_hasel += 1
+    elif i[2][a-1] != szukana and i[2][b-1] == szukana:
+        ilosc_hasel += 1
+    else:
+        continue
+
+
+print(ilosc_hasel)
